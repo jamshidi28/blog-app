e7334d1c09e6c158e85ee15d26c589d4bb747444
Jamshidi <mj.jamshidi2866@gmail.com>
2025-02-09 10:18:51 +0330

    creat dashboard layout

--- src/app/(dashboard)/profile/page.jsx
+++ src/app/(dashboard)/profile/page.jsx
@@ -1,5 +1,15 @@
-function Profile() {
-  return <div>Profile</div>;
-}
 
+
+async function Profile() {
+  return (
+    <div>
+      <h1 className="text-xl mb-8 text-secondary-500">داشبورد</h1>
+      
+      <div>
+        <h1 className="text-xl mb-4 text-secondary-500">آخرین پست ها</h1>
+       
+      </div>
+    </div>
+  );
+}
 export default Profile;
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import vazirFont from "@/constants/localFont";
-import Header from "@/components/Header";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "@/context/AuthContext";
 
@@ -16,10 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${vazirFont.variable} font-sans min-h-screen`}>
-        <AuthProvider>
           <Toaster />
-          <Header />
-          <div className="container xl:max-w-screen-xl">{children}</div>
+        <AuthProvider>
+          {children}
         </AuthProvider>
       </body>
     </html>
