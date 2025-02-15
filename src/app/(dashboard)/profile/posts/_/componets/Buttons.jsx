6de0573ca116993277f5e51642b7a8d94593ddb0
Jamshidi <mj.jamshidi2866@gmail.com>
2025-02-15 10:38:52 +0330

    delete post

--- src/app/(dashboard)/profile/posts/_/componets/Buttons.jsx
+++ src/app/(dashboard)/profile/posts/_/componets/Buttons.jsx
@@ -1,31 +1,57 @@
 "use client"
-import Button from "@/ui/Button";
 import ButtonIcon from "@/ui/ButtonIcon";
+import ConfirmDelete from "@/ui/ConfirmDelete";
+import Modal from "@/ui/Modal";
 import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useState } from "react";
+import useDeletePost from "../../create/_/useDeletePost";
+import { useRouter } from "next/navigation";
 
-export function CreatPost(){
+export function CreatPost() {
     return (
         <Link
-          href="/profile/posts/create"
-          className="justify-self-end flex gap-x-4 py-3 items-center rounded-lg bg-primary-900 px-4 text-sm font-medium text-secondary-0 
+            href="/profile/posts/create"
+            className="justify-self-end flex gap-x-4 py-3 items-center rounded-lg bg-primary-900 px-4 text-sm font-medium text-secondary-0 
           transition-colors hover:bg-primary-700"
         >
-          <span className="hidden md:block">ایجاد پست</span>{" "}
-          <PlusIcon className="w-5" />
+            <span className="hidden md:block">ایجاد پست</span>{" "}
+            <PlusIcon className="w-5" />
         </Link>
-      );
+    );
 }
 
-export function DeletePost({ id }) {
+export function DeletePost({ post: { _id:id, title } }) {
+    const  [open, setOpen]  = useState();
+    const {deletePost,isDeleting}= useDeletePost()
+    const router = useRouter();
+
     return (
-        <div>
-            <ButtonIcon variant="outline" onClick={() => {
-                console.log(id)
-            }}>
+        <>
+            <ButtonIcon variant="outline" onClick={() => setOpen(true)} >
                 <TrashIcon className="text-error" />
             </ButtonIcon>
-        </div>
+
+            <Modal title={`حذف ${title}`} open={open} 
+            onClose={() => setOpen(false)}>
+
+               <ConfirmDelete
+                    resourceName={title}
+                    onClose={() => setOpen(false)}
+                    onConfirm ={(e) => {
+                        e.preventDefault();
+                        deletePost({id},{
+                            onSuccess :()=>{
+                                setOpen(false);
+                                router.refresh("/profile/posts")
+                            }
+                        })
+                    
+                    }}
+                    disabled={isDeleting}
+                />
+            </Modal>
+        </>
     )
 }
 
--- src/app/(dashboard)/profile/posts/_/componets/PostRow.jsx
+++ src/app/(dashboard)/profile/posts/_/componets/PostRow.jsx
@@ -33,7 +33,7 @@ function PostRow({ post, index }) {
             <td>{
                 <div className='flex items-center gap-x-3'>
                     <UpdatePost id={post._id} />
-                    <DeletePost id={post._id} />
+                    <DeletePost post={post} />
                 </div>
             }</td>
         </Table.Row>
